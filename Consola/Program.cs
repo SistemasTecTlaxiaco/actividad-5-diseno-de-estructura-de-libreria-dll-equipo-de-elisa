49e12979207152b80a7193a233d3751fe1ab4d88
yanet-02 <jenamoradagarcia@gmail.com>
2025-03-18 12:49:58 -0600

    Agregue logaritmo base 10

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -17,7 +17,7 @@ namespace Consola
             {
                 try
                 {
-                    Console.WriteLine("-----Menu Principal Tec de Tlaxiaco1-----------");
+                    Console.WriteLine("-----Menu Principal Tec de Tlaxiaco1---");
                     Console.WriteLine("1. Sumar dos numeros");
                     Console.WriteLine("2. Restar");
                     Console.WriteLine("3. Multiplicar");
